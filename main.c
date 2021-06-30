7d0dd274c8c6d6bff1bedfa8cb834ed1cddd861d
Gustavo Buzogany Eboli <gbuzogany@gmail.com>
2021-06-30 14:12:00 +0200

    Progress on usb audio - reduced latency

--- main.c
+++ main.c
@@ -105,26 +105,6 @@ static void mic_audio_user_ev_handler(app_usbd_class_inst_t const * p_inst,
 /**
  * @brief   Input terminal channel configuration
  */
-#define HP_TERMINAL_CH_CONFIG()                                                                       \
-        (APP_USBD_AUDIO_IN_TERM_CH_CONFIG_LEFT_FRONT | APP_USBD_AUDIO_IN_TERM_CH_CONFIG_RIGHT_FRONT)
-
-/**
- * @brief   Feature controls
- *
- *      general
- *      channel 0
- *      channel 1
- */
-#define HP_FEATURE_CONTROLS()                                               \
-        APP_USBD_U16_TO_RAW_DSC(APP_USBD_AUDIO_FEATURE_UNIT_CONTROL_MUTE),  \
-        APP_USBD_U16_TO_RAW_DSC(APP_USBD_AUDIO_FEATURE_UNIT_CONTROL_MUTE),  \
-        APP_USBD_U16_TO_RAW_DSC(APP_USBD_AUDIO_FEATURE_UNIT_CONTROL_MUTE)
-
-
-
-/**
- * @brief   Input terminal channel configuration
- */
 #define MIC_TERMINAL_CH_CONFIG()                                                                       \
         (APP_USBD_AUDIO_IN_TERM_CH_CONFIG_LEFT_FRONT)
 
@@ -152,7 +132,7 @@ APP_USBD_AUDIO_FORMAT_DESCRIPTOR(m_mic_form_desc,
                                     2,                              /* Subframe size */
                                     16,                             /* Bit resolution */
                                     1,                              /* Frequency type */
-                                    APP_USBD_U24_TO_RAW_DSC(44100)) /* Frequency */
+                                    APP_USBD_U24_TO_RAW_DSC(47619)) /* Frequency */
                                 );
 
 /**
@@ -186,57 +166,7 @@ APP_USBD_AUDIO_FEATURE_DESCRIPTOR(m_mic_fea_desc,
                                     MIC_FEATURE_CONTROLS()) /* List of controls */
                                  );
 
-/* Headphones descriptors */
-
-/**
- * @brief   Audio class specific format III descriptor
- */
-// APP_USBD_AUDIO_FORMAT_DESCRIPTOR(m_hp_form_desc, 
-//                                  APP_USBD_AUDIO_AS_FORMAT_III_DSC(    /* Format type 3 descriptor */
-//                                     2,                                /* Number of channels */
-//                                     2,                                /* Subframe size */
-//                                     16,                               /* Bit resolution */
-//                                     1,                                /* Frequency type */
-//                                     APP_USBD_U24_TO_RAW_DSC(48000))   /* Frequency */
-//                                 );
-
-/**
- * @brief   Audio class input terminal descriptor
- */
-// APP_USBD_AUDIO_INPUT_DESCRIPTOR(m_hp_inp_desc, 
-//                                 APP_USBD_AUDIO_INPUT_TERMINAL_DSC(
-//                                     1,                                     /* Terminal ID */
-//                                     APP_USBD_AUDIO_TERMINAL_USB_STREAMING, /* Terminal type */
-//                                     2,                                     /* Number of channels */
-//                                     HP_TERMINAL_CH_CONFIG())               /* Channels config */
-//                                );
-
-/**
- * @brief   Audio class output terminal descriptor
- */
-// APP_USBD_AUDIO_OUTPUT_DESCRIPTOR(m_hp_out_desc, 
-//                                  APP_USBD_AUDIO_OUTPUT_TERMINAL_DSC(
-//                                     3,                                      /* Terminal ID */
-//                                     APP_USBD_AUDIO_TERMINAL_OUT_HEADPHONES, /* Terminal type */
-//                                     2)                                      /* Source ID */
-//                                 );
-
-/**
- * @brief   Audio class feature unit descriptor
- */
-// APP_USBD_AUDIO_FEATURE_DESCRIPTOR(m_hp_fea_desc, 
-//                                   APP_USBD_AUDIO_FEATURE_UNIT_DSC(
-//                                     2,                     /* Unit ID */
-//                                     1,                     /* Source ID */
-//                                     HP_FEATURE_CONTROLS()) /* List of controls */
-//                                  );
-
 /* Interfaces lists */
-
-/**
- * @brief Interfaces list passed to @ref APP_USBD_AUDIO_GLOBAL_DEF
- */
-// #define HP_INTERFACES_CONFIG() APP_USBD_AUDIO_CONFIG_OUT(0, 1)
 
 /**
  * @brief Interfaces list passed to @ref APP_USBD_AUDIO_GLOBAL_DEF
@@ -244,26 +174,6 @@ APP_USBD_AUDIO_FEATURE_DESCRIPTOR(m_mic_fea_desc,
 #define MIC_INTERFACES_CONFIG() APP_USBD_AUDIO_CONFIG_IN(2, 3)
 
 /*lint -save -e26 -e64 -e123 -e505 -e651*/
-
-
-// /**
-//  * @brief Headphone Audio class instance
-//  */
-// APP_USBD_AUDIO_GLOBAL_DEF(m_app_audio_headphone,
-//                           HP_INTERFACES_CONFIG(),
-//                           hp_audio_user_ev_handler,
-//                           &m_hp_form_desc,
-//                           &m_hp_inp_desc,
-//                           &m_hp_out_desc,
-//                           &m_hp_fea_desc,
-//                           0,
-//                           APP_USBD_AUDIO_AS_IFACE_FORMAT_PCM,
-//                           192,
-//                           APP_USBD_AUDIO_SUBCLASS_AUDIOSTREAMING,
-//                           1
-// );
-
-
 
 /**
  * @brief Microphone Audio class instance
@@ -282,13 +192,11 @@ APP_USBD_AUDIO_GLOBAL_DEF(m_app_audio_microphone,
                           3
 );
 
-
 /*lint -restore*/
 
 /**
  * @brief Internal audio temporary buffer
  */
-// static int16_t  m_temp_buffer[2 * BUFFER_SIZE];
 
 #define I2S_DATA_BLOCK_WORDS 64
 static uint32_t m_buffer_rx[2][I2S_DATA_BLOCK_WORDS];
@@ -305,21 +213,13 @@ static uint32_t buffer_length = 0;
 static uint8_t volatile m_blocks_transferred     = 0;
 static uint8_t          m_zero_samples_to_ignore = 2;
 static bool             m_error_encountered;
+static bool             m_recording = false;
+static uint32_t         m_tx_cnt    = 0;
 
 /**
  * @brief The size of last received block from the microphone
  */
 // static size_t m_temp_buffer_size;
-
-/**
- * @brief Actual headphones mute
- */
-// static uint8_t  m_mute_hp;
-
-/**
- * @brief Actual sampling frequency
- */
-// static uint32_t m_freq_hp;
 
 /**
  * @brief Actual microphone mute state
@@ -330,53 +230,6 @@ static uint8_t  m_mute_mic;
  * @brief Actual microphone sampling frequency
  */
 static uint32_t m_freq_mic;
-
-/**
- * @brief Audio class specific request handle (headphones)
- */
-// static void hp_audio_user_class_req(app_usbd_class_inst_t const * p_inst)
-// {
-//     app_usbd_audio_t const * p_audio = app_usbd_audio_class_get(p_inst);
-//     app_usbd_audio_req_t * p_req = app_usbd_audio_class_request_get(p_audio);
-
-//     UNUSED_VARIABLE(m_mute_hp);
-//     UNUSED_VARIABLE(m_freq_hp);
-
-//     switch (p_req->req_target)
-//     {
-//         case APP_USBD_AUDIO_CLASS_REQ_IN:
-
-//             if (p_req->req_type == APP_USBD_AUDIO_REQ_SET_CUR)
-//             {
-//                 //Only mute control is defined
-//                 p_req->payload[0] = m_mute_hp;
-//             }
-
-//             break;
-//         case APP_USBD_AUDIO_CLASS_REQ_OUT:
-
-//             if (p_req->req_type == APP_USBD_AUDIO_REQ_SET_CUR)
-//             {
-//                 //Only mute control is defined
-//                 m_mute_hp = p_req->payload[0];
-//             }
-
-//             break;
-//         case APP_USBD_AUDIO_EP_REQ_IN:
-//             break;
-//         case APP_USBD_AUDIO_EP_REQ_OUT:
-
-//             if (p_req->req_type == APP_USBD_AUDIO_REQ_SET_CUR)
-//             {
-//                 //Only set frequency is supported
-//                 m_freq_hp = uint24_decode(p_req->payload);
-//             }
-
-//             break;
-//         default:
-//             break;
-//     }
-// }
 
 /**
  * @brief Audio class specific request handle (microphone)
@@ -426,35 +279,6 @@ static void mic_audio_user_class_req(app_usbd_class_inst_t const * p_inst)
 }
 
 /**
- * @brief User event handler @ref app_usbd_audio_user_ev_handler_t (headphones)
- */
-// static void hp_audio_user_ev_handler(app_usbd_class_inst_t const * p_inst,
-//                                      app_usbd_audio_user_event_t   event)
-// {
-//     app_usbd_audio_t const * p_audio = app_usbd_audio_class_get(p_inst);
-//     UNUSED_VARIABLE(p_audio);
-//     switch (event)
-//     {
-//         case APP_USBD_AUDIO_USER_EVT_CLASS_REQ:
-//             hp_audio_user_class_req(p_inst);
-//             break;
-//         case APP_USBD_AUDIO_USER_EVT_RX_DONE:
-//         {
-//             ret_code_t ret;
-//             /* Block from headphones copied into buffer, send it into microphone input */
-//             ret = app_usbd_audio_class_tx_start(&m_app_audio_microphone.base, m_temp_buffer, m_temp_buffer_size);
-//             if (NRF_SUCCESS == ret)
-//             {
-//                 bsp_board_led_invert(LED_AUDIO_RX);
-//             }
-//             break;
-//         }
-//         default:
-//             break;
-//     }
-// }
-
-/**
  * @brief User event handler @ref app_usbd_audio_user_ev_handler_t (microphone)
  */
 static void mic_audio_user_ev_handler(app_usbd_class_inst_t const * p_inst,
@@ -478,27 +302,6 @@ static void mic_audio_user_ev_handler(app_usbd_class_inst_t const * p_inst,
     }
 }
 
-// static void hp_sof_ev_handler(uint16_t framecnt)
-// {
-//     UNUSED_VARIABLE(framecnt);
-//     if (APP_USBD_STATE_Configured != app_usbd_core_state_get())
-//     {
-//         return;
-//     }
-//     size_t rx_size = app_usbd_audio_class_rx_size_get(&m_app_audio_headphone.base);
-//     m_temp_buffer_size = rx_size;
-//     if (rx_size > 0)
-//     {
-//         ASSERT(rx_size <= sizeof(m_temp_buffer));
-//         ret_code_t ret;
-//         ret = app_usbd_audio_class_rx_start(&m_app_audio_headphone.base, m_temp_buffer, rx_size);
-//         if (NRF_SUCCESS != ret)
-//         {
-//             NRF_LOG_ERROR("Cannot start RX transfer from headphone\r\n");
-//         }
-//     }
-// }
-
 /**
  * @brief USBD library specific event handler.
  *
@@ -514,8 +317,12 @@ static void usbd_user_ev_handler(app_usbd_event_type_t event)
                 break;
             }
 
-            int len = 45;
-            if (buffer_length >= len) {
+            if (buffer_length >= 47) {
+                uint32_t len = 47;
+                if (buffer_length > 47*4) {
+                    len = 48;
+                }
+                
                 uint16_t buf[len];
                 for (int i=0;i<len;i++) {
                     buf[i] = m_audio_buffer[read_pos++];
@@ -523,26 +330,38 @@ static void usbd_user_ev_handler(app_usbd_event_type_t event)
                         read_pos -= AUDIO_BUF_SIZE;
                     }
                 }
-                
+                // read_pos = write_pos;
+                // buffer_length = 0;
+                buffer_length -= len;
+
                 /* Block from headphones copied into buffer, send it into microphone input */
                 ret_code_t ret = app_usbd_audio_class_tx_start(&m_app_audio_microphone.base, buf, len * 2);
                 if (NRF_SUCCESS == ret)
                 {
                     bsp_board_led_invert(LED_AUDIO_TX);
-                    buffer_length -= len;
                 }
+            }
+            else {
+                m_tx_cnt += 1;
             }
             break;
         case APP_USBD_EVT_DRV_SUSPEND:
             bsp_board_leds_off();
+            m_recording = false;
+            NRF_LOG_INFO("USB suspend");
             break;
         case APP_USBD_EVT_DRV_RESUME:
+            NRF_LOG_INFO("USB resume");
+            m_recording = true;
             // bsp_board_led_on(LED_USB_RESUME);
             break;
         case APP_USBD_EVT_STARTED:
+            NRF_LOG_INFO("USB started");
             // bsp_board_led_on(LED_USB_START);
             break;
         case APP_USBD_EVT_STOPPED:
+            NRF_LOG_INFO("USB stopped");
+            m_recording = false;
             app_usbd_disable();
             bsp_board_leds_off();
             break;
@@ -603,6 +422,30 @@ static bool check_samples(uint32_t const * p_block)
     return true;
 }
 
+static void check_rx_data(uint32_t const * p_block)
+{
+    if (m_blocks_transferred < m_zero_samples_to_ignore) {
+        ++m_blocks_transferred;
+    }
+
+    if (!m_error_encountered && m_recording)
+    {
+        m_error_encountered = !check_samples(p_block);
+    }
+
+    if (m_error_encountered)
+    {
+        NRF_LOG_INFO("error?");
+        // bsp_board_led_off(LED_OK);
+        // bsp_board_led_invert(LED_ERROR);
+    }
+    else
+    {
+        // NRF_LOG_INFO("rx");
+        // bsp_board_led_off(LED_ERROR);
+        bsp_board_led_invert(BSP_BOARD_LED_1);
+    }
+}
 
 static void data_handler(nrf_drv_i2s_buffers_t const * p_released,
                          uint32_t                      status)
@@ -634,7 +477,8 @@ static void data_handler(nrf_drv_i2s_buffers_t const * p_released,
     }
     else
     {
-        mp_block_to_check = p_released->p_rx_buffer;
+        // mp_block_to_check = p_released->p_rx_buffer;
+        check_rx_data(p_released->p_rx_buffer);
         // The driver has just finished accessing the buffers pointed by
         // 'p_released'. They can be used for the next part of the transfer
         // that will be scheduled now.
@@ -643,32 +487,6 @@ static void data_handler(nrf_drv_i2s_buffers_t const * p_released,
             .p_tx_buffer = NULL,
         };
         APP_ERROR_CHECK(nrf_drv_i2s_next_buffers_set(&next_buffers));
-    }
-}
-
-
-static void check_rx_data(uint32_t const * p_block)
-{
-    if (m_blocks_transferred < m_zero_samples_to_ignore) {
-        ++m_blocks_transferred;
-    }
-
-    if (!m_error_encountered)
-    {
-        m_error_encountered = !check_samples(p_block);
-    }
-
-    if (m_error_encountered)
-    {
-        NRF_LOG_INFO("error?");
-        // bsp_board_led_off(LED_OK);
-        // bsp_board_led_invert(LED_ERROR);
-    }
-    else
-    {
-        // NRF_LOG_INFO("rx");
-        // bsp_board_led_off(LED_ERROR);
-        bsp_board_led_invert(BSP_BOARD_LED_1);
     }
 }
 
@@ -681,8 +499,8 @@ void i2s_init()
     config.sck_pin   = I2S_CONFIG_SCK_PIN;
     config.sdout_pin = NRFX_I2S_PIN_NOT_USED;
     config.mck_pin   = NRFX_I2S_PIN_NOT_USED;
-    config.mck_setup = NRF_I2S_MCK_32MDIV11;
-    config.ratio     = NRF_I2S_RATIO_64X;
+    config.mck_setup = NRF_I2S_MCK_32MDIV21;
+    config.ratio     = NRF_I2S_RATIO_32X;
     config.channels  = NRF_I2S_CHANNELS_LEFT;
     config.mode      = NRF_I2S_MODE_MASTER;
     config.sample_width = NRF_I2S_SWIDTH_16BIT;
@@ -736,7 +554,7 @@ int main(void)
     APP_ERROR_CHECK(ret);
 
     app_usbd_class_inst_t const * class_inst_mic =
-        app_usbd_audio_class_inst_get(&m_app_audio_microphone);
+    app_usbd_audio_class_inst_get(&m_app_audio_microphone);
     ret = app_usbd_class_append(class_inst_mic);
     APP_ERROR_CHECK(ret);
 
@@ -783,12 +601,16 @@ int main(void)
         
         if (mp_block_to_check)
         {
-            check_rx_data(mp_block_to_check);
-            mp_block_to_check = NULL;
+            // check_rx_data(mp_block_to_check);
+            // mp_block_to_check = NULL;
         }
     
         if (i % 1000 == 0) {
-            NRF_LOG_INFO("Buffer: %d", buffer_length);
+            if (m_tx_cnt > 0) {
+                NRF_LOG_INFO("TX Failed: %d", m_tx_cnt);
+                m_tx_cnt = 0;
+            }
+            // NRF_LOG_INFO("Buffer: %d", buffer_length);
             i = 0;
         }
 
